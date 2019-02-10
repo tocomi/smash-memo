60a847a90036bbe2769956800f411d304a4c47ce
tocomi <tocomi@wish.ocn.ne.jp>
2019-02-10 16:54:49 +0900

    add function to add item

--- App.js
+++ App.js
@@ -6,24 +6,66 @@ import {
   StatusBar,
   Platform,
   ScrollView,
+  FlatList,
+  TextInput,
+  Button,
+  KeyboardAvoidingView,
 } from 'react-native';
 
-const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 40 : StatusBar.currentHeigh
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      memo: [],
+      currendIndex: 0,
+      inputText: "",
+    }
+  }
+
+  onAddItem() {
+    const title = this.state.inputText
+    if (title === "") {
+      return;
+    }
+    const index = this.state.currendIndex
+    const newMemo = { index: index, title: title }
+    const memo = [...this.state.memo, newMemo]
+    this.setState({
+      memo: memo,
+      currendIndex: index + 1,
+      inputText: "",
+    })
+  }
+
   render() {
     return (
-      <View style={styles.container}>
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
         <View style={styles.filter}>
           <Text>Filter</Text>
         </View>
-        <ScrollView style={styles.todoList}>
-          <Text>Todo</Text>
+        <ScrollView style={styles.memoList}>
+          <FlatList
+            data={this.state.memo}
+            renderItem={({item}) => <Text>{item.title}</Text>}
+            keyExtrator={(item, index) => item.index}
+          />
         </ScrollView>
         <View style={styles.input}>
-          <Text>Input</Text>
+          <TextInput
+            onChangeText={(text) => this.setState({inputText: text})}
+            value={this.state.inputText}
+            style={styles.inputText}
+          />
+          <Button
+            onPress={this.onAddItem}
+            title="Add"
+            style={styles.inputButton}
+          />
         </View>
-      </View>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -37,10 +79,18 @@ const styles = StyleSheet.create({
   filter: {
     height: 30,
   },
-  todoList: {
+  memoList: {
     flex: 1,
   },
   input: {
     height: 30,
+    flexDirection: 'row',
   },
+  inputText: {
+    flex: 1,
+  },
+  inputButton: {
+    width: 100,
+    color: '#841584',
+  }
 });
