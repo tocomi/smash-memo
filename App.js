69b35f066c097e5de22dc1eb08a5b7769df65cd6
tocomi <tocomi@wish.ocn.ne.jp>
2019-02-15 01:26:00 +0900

    prepare redux

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import MemoScreen from './src/MemoScreen'
+import { Provider } from 'react-redux'
+import { store } from './src/store/store'
 
 export default class App extends React.Component {
   render() {
     return (
-      <MemoScreen/>
+      <Provider store={store}>
+        <MemoScreen/>
+      </Provider>
     )
   }
 }
