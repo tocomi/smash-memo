b8d21d0213d11bd85844aafef776518bcd436f7e
tocomi <tocomi@wish.ocn.ne.jp>
2019-03-02 21:53:04 +0900

    add 64 characters

--- src/data/characters.js
+++ src/data/characters.js
@@ -12,7 +12,43 @@ export const characters = [
     image: require('../../assets/character/donkey.jpg'),
   },
   {
+    name: 'link',
+    image: require('../../assets/character/link.jpg'),
+  },
+  {
+    name: 'samus',
+    image: require('../../assets/character/samus.jpg'),
+  },
+  {
+    name: 'yoshi',
+    image: require('../../assets/character/yoshi.jpg'),
+  },
+  {
+    name: 'kirby',
+    image: require('../../assets/character/kirby.jpg'),
+  },
+  {
+    name: 'fox',
+    image: require('../../assets/character/fox.jpg'),
+  },
+  {
+    name: 'pikachu',
+    image: require('../../assets/character/pikachu.jpg'),
+  },
+  {
+    name: 'luigi',
+    image: require('../../assets/character/luigi.jpg'),
+  },
+  {
+    name: 'ness',
+    image: require('../../assets/character/ness.jpg'),
+  },
+  {
     name: 'falcon',
     image: require('../../assets/character/falcon.jpg'),
+  },
+  {
+    name: 'purin',
+    image: require('../../assets/character/purin.jpg'),
   },
 ]
